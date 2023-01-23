cbeb6c90ff4af6b3f482a0314cf01bd8742bba5b
mike spindel <mike@spindel.is>
2023-01-22 18:50:32 -0800

    Update run-ida.sh to support recent versions of IDA
    Uses LD_PRELOAD to avoid the need for idapyswitch

--- run-ida.sh
+++ run-ida.sh
@@ -22,17 +22,37 @@ case $name in
         exit 1;
 esac
 
+# Look for the appropriate ida binary in $IDAHOME
 ida_bin=("$IDAHOME"/$ida_pat)
 if [[ ! -e "$ida_bin" ]]; then
     echo "IDA binary not found: $ida_bin"
     exit 1
 fi
 
+# Check if this is actually an idaenv install
+if [[ ! -e "$IDAENV/bin/idaenv" ]]; then
+    echo "idaenv not found in virtual environment"
+    # Start IDA without further effort
+    "$ida_bin" "$@"
+    exit
+fi
+
 # Activate the virtualenv
 source "$IDAENV/bin/activate"
 
-# Configure environment
-export IDAUSR="$HOME/.idapro:$( idaenv prefix )"
+# Configure IDAUSR
+export IDAUSR="$HOME/.idapro:$( "$IDAENV/bin/idaenv" prefix )"
 
-# Start the target binary
-"$ida_bin" "$@"
+# Try to find libpython for preload
+LIBPYTHON="$(python -c 'import sysconfig; print("%s/%s" % (
+       sysconfig.get_config_var("LIBPL"),
+       sysconfig.get_config_var("LDLIBRARY")))' 2>&1)"
+
+# Start the target IDA binary
+if [[ -f "$LIBPYTHON" && -f "/lib64/ld-linux-x86-64.so.2" ]]; then
+    # Preload correct libpython
+    LD_PRELOAD="$LIBPYTHON" "$ida_bin" "$@"
+else
+    # Hope for the best
+    "$ida_bin" "$@"
+fi
